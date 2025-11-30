a2d841be7322d8fb50cfa5e8b7a95ce17eb19fd6
vanderbr <11340250+vanderbr@users.noreply.github.com>
2025-11-30 21:41:33 +0100

    Update Brotli.swift

--- Sources/DidmeKit/Utils/Brotli.swift
+++ Sources/DidmeKit/Utils/Brotli.swift
@@ -23,12 +23,52 @@ import Compression
 public enum DIDBrotli {
 
     public static func compress(_ data: Data) throws -> Data {
-        let compressor = try BrotliCompressor(mode: .generic, quality: 11)
-        return try compressor.compress(data)
+        try perform(operation: COMPRESSION_STREAM_ENCODE,
+                    algorithm: COMPRESSION_BROTLI,
+                    data: data)
     }
 
     public static func decompress(_ data: Data) throws -> Data {
-        let decompressor = BrotliDecompressor()
-        return try decompressor.decompress(data)
+        try perform(operation: COMPRESSION_STREAM_DECODE,
+                    algorithm: COMPRESSION_BROTLI,
+                    data: data)
+    }
+
+    private static func perform(operation: compression_stream_operation,
+                                algorithm: compression_algorithm,
+                                data: Data) throws -> Data {
+
+        var stream = compression_stream()
+        var status = compression_stream_init(&stream, operation, algorithm)
+        guard status != COMPRESSION_STATUS_ERROR else {
+            throw NSError(domain: "brotli", code: -1)
+        }
+        defer { compression_stream_destroy(&stream) }
+
+        let dstBufferSize = 64 * 1024
+        let dstBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: dstBufferSize)
+        defer { dstBuffer.deallocate() }
+
+        return data.withUnsafeBytes { srcRawBuffer in
+            var result = Data()
+            stream.src_ptr  = srcRawBuffer.bindMemory(to: UInt8.self).baseAddress!
+            stream.src_size = srcRawBuffer.count
+            stream.dst_ptr  = dstBuffer
+            stream.dst_size = dstBufferSize
+
+            repeat {
+                status = compression_stream_process(&stream, 0)
+
+                let written = dstBufferSize - stream.dst_size
+                if written > 0 {
+                    result.append(dstBuffer, count: written)
+                }
+
+                stream.dst_ptr = dstBuffer
+                stream.dst_size = dstBufferSize
+            } while status == COMPRESSION_STATUS_OK
+
+            return result
+        }
     }
 }
